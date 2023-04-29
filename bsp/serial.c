1e78d18829f43943aac1e0892a352ee4be11cf1c
shravista <shravistakashyap@gmail.com>
2023-04-29 11:40:58 +0530

    init

--- bsp/serial.c
+++ bsp/serial.c
@@ -1,158 +1,138 @@
 #include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include "servo.h"
-
-// #define concat(a, b) a##b
-/*
- * We will use PWM to provide a square wave to the speaker.
- * We can play various tones by setting the frequency of PWM.
- */
+#include "gpio.h"
+#include "serial.h"
 
 #ifndef IOREG32
 #define IOREG32(addr) (*(volatile unsigned long *) (addr))
 #endif
 
 /*
- * NRF52 has a PWM that has more functionality than what we need.
- * Hence, we are using a subset of the PWM registers here.
+ * UART Registers:
+ * 1. Tasks, also called command registers to start, stop and enable/disable
+ * 2. Events
+ * 3. Pin select (which pins connect to rx/tx)
+ * 4. Data
+ * 5. Configuration (baud rate, parity)
  */
 
-/* Register offsets */
-#define STOP_PWM_A       		  IOREG32(0x40021004)     // stop pwm
-#define SEQ0START_PWM_A  		  IOREG32(0x40021008)     // start sequence 0
-#define EVENTS_STOPPED_PWM_A     IOREG32(0x40021104)     // event to indicate PWM stopped
-#define EVENTS_SEQ0END_PWM_A     IOREG32(0x40021110)     // event to indicate sequnce 0 ended
-#define EVENTS_PWMEND_PWM_A     IOREG32(0x40021118)     // event to indicate pwm period ended
-#define ENABLE_PWM_A             IOREG32(0x40021500)     // enable / disable pwm
-#define MODE_PWM_A               IOREG32(0x40021504)     // countertop mode up/upDown
-#define COUNTERTOP_PWM_A         IOREG32(0x40021508)     //   countertop = pwm clock / pwm freq
-#define PRESCALER_PWM_A          IOREG32(0x4002150C)     // divisor for PWM clock (2 power n)
-#define DECODER_PWM_A          IOREG32(0x40021510)
-#define SEQ0PTR_PWM_A            IOREG32(0x40021520)     // address where the sequence is stored
-#define SEQ0CNT_PWM_A            IOREG32(0x40021524)     // number of values in the sequence
-#define SEQ0REFRESH_PWM_A        IOREG32(0x40021528)     // additional pwm cycles between two values of the sequence
-#define SEQ0ENDDELAY_PWM_A       IOREG32(0x4002152C)     // additional pwm cycles at the end
-#define DECODER_PWM_A            IOREG32(0x40021510)     
-// to set indivudial duty cycles of all 4 PWM channels we will use common configuration to set all 4 PWM to same duty cycle                                                   
-#define PSEL_PWM_A               IOREG32(0x40021560)     // pin select
+/* 1. Task registers */
+#define UART_STARTRX   		IOREG32(0x40002000)        // start rx
+#define UART_STARTTX   		IOREG32(0x40002008)        // start tx
+#define UART_ENABLE    		IOREG32(0x40002500)        // enable/disable
+#define   UART_ENABLE_Disabled  0
+#define   UART_ENABLE_Enabled   4
 
-#define PSEL_PWM_B               IOREG32(0x40022560)     // pin select
-#define STOP_PWM_B       		 IOREG32(0x40022004)     // stop pwm
-#define SEQ0START_PWM_B  		 IOREG32(0x40022008)     // start sequence 0
-#define EVENTS_STOPPED_PWM_B     IOREG32(0x40022104)     // event to indicate PWM stopped
-#define EVENTS_SEQ0END_PWM_B     IOREG32(0x40022110)     // event to indicate sequnce 0 ended
-#define EVENTS_PWMEND_PWM_B      IOREG32(0x40022118)     // event to indicate pwm period ended
-#define ENABLE_PWM_B             IOREG32(0x40022500)     // enable / disable pwm
-#define MODE_PWM_B               IOREG32(0x40022504)     // countertop mode up/upDown
-#define COUNTERTOP_PWM_B         IOREG32(0x40022508)     //   countertop = pwm clock / pwm freq
-#define PRESCALER_PWM_B          IOREG32(0x4002250C)     // divisor for PWM clock (2 power n)
-#define DECODER_PWM_B            IOREG32(0x40022510)
-#define SEQ0PTR_PWM_B            IOREG32(0x40022520)     // address where the sequence is stored
-#define SEQ0CNT_PWM_B            IOREG32(0x40022524)     // number of values in the sequence
-#define SEQ0REFRESH_PWM_B        IOREG32(0x40022528)     
-#define SEQ0ENDDELAY_PWM_B       IOREG32(0x4002252C)     // additional pwm cycles at the end
-#define DECODER_PWM_B            IOREG32(0x40022510)     
+/* 2. Event registers */
+#define UART_RXDRDY    		IOREG32(0x40002108)        // rx ready flag
+#define UART_TXDRDY    		IOREG32(0x4000211c)        // tx ready flag
 
-#define   PRESCALER_DIV16   4                       // 0.5MHz clock
-#define PWM_CLK 1000000
+/* 3. Tx and Rx pin select registers:
+ * pin numbers are defined as port*32 + pin
+ * for example, P0_6 = 6, P1_6 = 38
+ */
+#define UART_PSELTXD   		IOREG32(0x4000250c)        // tx pin number
+#define UART_PSELRXD   		IOREG32(0x40002514)        // rx pin number
 
-// static uint16_t s_sequenceA[2], s_sequenceB[2];
-static uint16_t s_sequence_A[1], s_sequence_B[1];
+/* 4. Data registers */
+#define UART_RXD       		IOREG32(0x40002518)
+#define UART_TXD       		IOREG32(0x4000251c)
 
-/* APIs */
-void servo_init(void){
-    PRESCALER_PWM_A = PRESCALER_DIV16;  // .5MHz clock
-    ENABLE_PWM_A = 1;
-    MODE_PWM_A = 0;     // only UP mode
-    // DECODER_PWM_A = 2;    // Individual Decoder Mode
+/* 5. Configurtion */
 
-    PRESCALER_PWM_B = PRESCALER_DIV16;
-    ENABLE_PWM_B = 1;
-    MODE_PWM_B = 0;     // only UP mode
-    // DECODER_PWM_B = 2;    // Individual Decoder Mode
+/* Baud rate register and values */
+#define UART_BAUDRATE  		IOREG32(0x40002524)
+#define   UART_BAUDRATE_115200 0x01d60000
+                                // There are many other baud rate defines
+                                // ranging from 9600 to 1M. We're using only
+                                // the one that we need.
 
-    PSEL_PWM_A = A_PIN;   //Connect the port to the pin
-    PSEL_PWM_B = B_PIN;    
+/* Flow control and parity */
+#define UART_CONFIG    		IOREG32(0x4000256c)
+#define   UART_CONFIG_HWFC_Off  0
+#define   UART_CONFIG_HWFC_On   1
+#define   UART_PARITY_None      0
+#define   UART_PARITY_Even      (7 << 1)
+
+void serial_init(uint32_t rxpin, uint32_t txpin)
+{
+    /* Disable while changing parameters */
+    UART_ENABLE = UART_ENABLE_Disabled;
+
+    /* Set up pins to maintain signal levels while UART disabled */
+    gpio_out(txpin, DRIVE_STANDARD);            // output
+    gpio_in(rxpin, PULL_NONE);  // input
+    gpio_set(txpin);            // set tx high
+
+    /* Connect UART to port pins */
+    UART_PSELTXD = txpin;
+    UART_PSELRXD = rxpin;
+
+    /* UART parameters */
+    UART_BAUDRATE = UART_BAUDRATE_115200;
+    UART_CONFIG = UART_CONFIG_HWFC_Off | UART_PARITY_None;
+
+    /* Configuration complete. Now enable the UART. */
+    UART_ENABLE = UART_ENABLE_Enabled;
+
+    /* Start Rx and Tx */
+    UART_STARTTX = 1;
+    UART_STARTRX = 1;
+
+    /* Clear Rx ready flag */
+    UART_RXDRDY = 0;
 }
-/*
-traj=1 => straight; dirn=1 => frwd; dirn=-1 => bwd;
-traj=0 => circle; dirn=1 => CCW; dirn=-1 => CW;
-dirn=0 => stop
-speed levels limited to 6
-*/ 
-void servo_out(int traj, int dirn, int speed ){
-    // printf("Start\n");
-    int freq = 250; // Also look at base, diff, speed levels
-    int base = 100, diff= 300; 
-    int seqA, seqB;
-   
-    uint16_t countertop;
-    countertop = PWM_CLK / freq;
-    int mid_counter = countertop/2;
-    COUNTERTOP_PWM_A = countertop;
-    COUNTERTOP_PWM_B = countertop;
 
-    SEQ0PTR_PWM_A = (uint32_t) s_sequence_A;
-    SEQ0CNT_PWM_A = 1;
-    SEQ0REFRESH_PWM_A = 0;
+char getc_nowait(void)
+{
+    return UART_RXD;
+}
 
-    SEQ0PTR_PWM_B = (uint32_t) s_sequence_B;
-    SEQ0CNT_PWM_B = 1;
-    SEQ0REFRESH_PWM_B = 0;
+char getc(void)
+{
+    char ch;
 
-    if(dirn == 0){
-        // seqB = mid_counter; seqA = mid_counter; //Stop
-        seqB = countertop; seqA = countertop; //Stop
-    }
-    else{
-        if(traj == 1){
-            if(dirn==1){    // Frwd
-                seqB = base + speed*diff;
-                seqA = seqB + mid_counter; 
-            }
-            else{   //bwd
-                seqB = base + speed*diff +mid_counter;
-                seqA = seqB -mid_counter; 
-            }
-        }
-        else if(traj==0){
-            if(dirn==1){    // CCW
-                seqB = base + speed*diff ;
-                seqA = seqB; 
-            }
-            else{   //CW
-                seqB = base + speed*diff +mid_counter;
-                seqA = seqB; 
-            }
-        }
-        else{
-            seqB = countertop; seqA = countertop;   //Stop
-        }
-    }
-    
-    /* Set count values for specified duty cycle 
-    ForA >2000 Vehicle Frwd */
-    s_sequence_A[0] = seqA;
-    s_sequence_B[0] = seqB;
-    // s_sequence_A[1] = (countertop * dutyA) ;
-    // s_sequence_B[1] = (countertop * dutyB) ;
-    
-    /* Start sequence 0 */
-    SEQ0START_PWM_A = 1;
-    SEQ0START_PWM_B = 1;
-
-    /* Wait for the sequence to complete */
-    while (EVENTS_SEQ0END_PWM_A == 0 || EVENTS_SEQ0END_PWM_B == 0)
+    /* Wait for a character to arrive on TTY */
+    while (UART_RXDRDY == 0)    // wait for the flag
         ;
-    EVENTS_SEQ0END_PWM_A = 0;
-    EVENTS_SEQ0END_PWM_B = 0;
 
-    STOP_PWM_A = 1;
-    STOP_PWM_B = 1;
-    while (EVENTS_STOPPED_PWM_A == 0 || EVENTS_STOPPED_PWM_B == 0)
+    /* Data received. Read it and clear the flag. */
+    ch = UART_RXD;
+    UART_RXDRDY = 0;
+
+    return ch;
+}
+
+void putc_nowait(char ch)
+{
+    UART_TXD = ch;
+}
+
+void putc(char ch)
+{
+    /* Write into data buffer and wait till tx complete */
+    UART_TXD = ch;
+    while (UART_TXDRDY == 0)
         ;
-    EVENTS_STOPPED_PWM_A = 0;
-    EVENTS_STOPPED_PWM_B = 0;
 
+    /* Clear the event */
+    UART_TXDRDY = 0;
+
+    return;
+}
+
+void puts(char s[])
+{
+    char *p;
+
+    for (p = s; *p; p++)
+        putc(*p);
+}
+
+/* printf_buf -- integrating Spivey's library code */
+void print_buf(char *buf, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+        putc(buf[i]);
 }
