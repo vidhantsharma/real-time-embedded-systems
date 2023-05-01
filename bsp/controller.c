d1f0145316a96cfc6231ff84f4ea425e448093fc
shravista <shravistakashyap@gmail.com>
2023-05-01 08:31:37 +0530

    controller update

--- bsp/controller.c
+++ bsp/controller.c
@@ -5,7 +5,13 @@
 #include "servo.h"
 #include "board.h"
 
+#define count_max 15
 int flag;
+float ang_des_old = -1000;
+
+static int count=0; 
+int flag = 0;
+
 void turn_ctrlr(int dirn, float ang_des, float ang_curr){ // Only turning
     /*
     servo_out(int traj, int dirn, int speed );
@@ -14,14 +20,21 @@ void turn_ctrlr(int dirn, float ang_des, float ang_curr){ // Only turning
     dirn=0 => stop
     speed levels limited to 6
     */ 
-    // printf("\n[CONTROLLER] TURN CONTROLLER");
-    float tolr = 15.0;
-    // float ang_curr = computeHeading();
-    // print_float_ctrl(abs(ang_des - ang_curr),4);
-    
-    if((abs(ang_des - ang_curr) >= tolr) && flag==1){
-    // if((abs(ang_des - ang_curr) >= tolr) ){
+    float tolr = 9.0;
+    // float search_limit = 30.0;
+    float err;
+
+    if(ang_des_old != ang_des){
+        flag = 1;
+        ang_des_old = ang_des;
+    }
+    err = ang_des - ang_curr ; 
+    if((abs(err) >= tolr) && flag==1){
         servo_out(0,dirn,3);
+        // if(abs(abs(err)-tolr) < search_limit)
+        //     servo_out(0,dirn,3);
+        // else
+        //     servo_out(0,-dirn,3);
         led_off(2,2);
     }
     else{
@@ -32,52 +45,77 @@ void turn_ctrlr(int dirn, float ang_des, float ang_curr){ // Only turning
     }
 }
 
-void move_ctrlr(int dirn, float orient, float val){ 
-    // printf("\n[CONTROLLER] MOVE CONTROLLER");
+void move_ctrlr(int dirn, float orient, float val){
     float err;
     // float val = computeHeading();
-    float tolr = 20.0;
+    float tolr = 12.0;
     flag = 1;
     if(dirn == 0)
         servo_out(1,0,3);
     else{
-        err = val - orient ; 
-        if(abs(err)<= tolr){
-            servo_out(1,dirn,3);
+        err = val - orient ;
+        if(count < count_max){
+            servo_out(1,dirn,3); 
+            count++;   
         }
-        else{
-            // if(abs(err) > 200){
-            //     if(err > 0){   
-            //         turn_ctrlr(1, orient,val); //oriented left, so command right
-            //         // servo_out(0,1,3);
-            //         printf("correcting to left");
-            //     }
-            //     else if(err < 0){
-            //         turn_ctrlr(-1, orient,val);
-            //         // servo_out(0,-1,3);
-            //         printf("correcting to ryt");
-            //     }   
-            // }
-            // else{
-                if( err > 0){   
-                    turn_ctrlr(-1, orient,val); //oriented left, so command right
-                    // servo_out(0,-1,3);
-                    // printf("correcting to ryt");
-                }
-                else if(err < 0){
-                    turn_ctrlr(1, orient,val);
-                    // servo_out(0,1,3);
-                    // printf("correcting to left");
-                }
-            // }
-        }
+        else if(abs(err)>= tolr){
+            if(err >= tolr)
+                turn_ctrlr(-1, orient, val);
+            else if(err < tolr)
+                turn_ctrlr(1, orient, val);
+        } 
+        else    
+            count = 0;     
     }
-    // with pins facing forward(180deg), left-ang increases, 
-    // Assuming "if" for smooth transition than "while"
-    
+
 }
 
-// void move_ctrlr()
+
+// void move_ctrlr(int dirn, float orient, float val){ 
+//     printf("\n[CONTROLLER] MOVE CONTROLLER");
+//     float err;
+//     // float val = computeHeading();
+//     float tolr = 20.0;
+//     flag = 1;
+//     if(dirn == 0)
+//         servo_out(1,0,3);
+//     else{
+//         err = val - orient ; 
+//         if(abs(err)<= tolr){
+//             servo_out(1,dirn,3);
+//         }
+//         else{
+//             // if(abs(err) > 200){
+//             //     if(err > 0){   
+//             //         turn_ctrlr(1, orient,val); //oriented left, so command right
+//             //         // servo_out(0,1,3);
+//             //         printf("correcting to left");
+//             //     }
+//             //     else if(err < 0){
+//             //         turn_ctrlr(-1, orient,val);
+//             //         // servo_out(0,-1,3);
+//             //         printf("correcting to ryt");
+//             //     }   
+//             // }
+//             // else{
+//                 if( err > 0){   
+//                     turn_ctrlr(-1, orient,val); //oriented left, so command right
+//                     // servo_out(0,-1,3);
+//                     printf("correcting to ryt");
+//                 }
+//                 else if(err < 0){
+//                     turn_ctrlr(1, orient,val);
+//                     // servo_out(0,1,3);
+//                     printf("correcting to left");
+//                 }
+//             // }
+//         }
+//     }
+//     // with pins facing forward(180deg), left-ang increases, 
+//     // Assuming "if" for smooth transition than "while"
+    
+// }
+
 
 
 // if(dirn != 0){
